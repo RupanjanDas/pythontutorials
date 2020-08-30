d585c4bfb8a423f88009fdb6450d352a050e6fc1
RupanjanDas <70481728+RupanjanDas@users.noreply.github.com>
2020-08-30 22:01:20 +0530

    Create revision1hw1.py
    My First Commit

---
+++ revision1hw1.py
@@ -0,0 +1,12 @@
+a = int(input("enter a number:"))
+b = int(input("enter a number:"))
+if a and b == 0:
+    print("error:division by 0")
+elif a == b:
+    print("error:division by 0")
+c = a + b
+d = a - b
+e = c / d
+f = d / c
+g = e * f
+print(g)
